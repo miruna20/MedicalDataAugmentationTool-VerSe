4d90866a4d86bbece18ce827be9186c70f892a21
miruna20 <50807607+miruna20@users.noreply.github.com>
2020-07-27 11:04:08 +0200

    training.sh used for experiment 1

--- docker/training.sh
+++ docker/training.sh
@@ -2,17 +2,14 @@
 
 export PYTHONPATH=/MedicalDataAugmentationTool
 
-mkdir /MedicalDataAugmentationTool-VerSe/verse2019_dataset/images/output
-
-#reorient all
 python /MedicalDataAugmentationTool-VerSe/other/reorient_reference_to_rai.py --image_folder /MedicalDataAugmentationTool-VerSe/verse2019_dataset/images  --output_folder /MedicalDataAugmentationTool-VerSe/verse2019_dataset/images_reoriented
 
 #run training scripts
 cd /MedicalDataAugmentationTool-VerSe/training
-mkdir /output
-python main_spine_localization.py 
+
+python main_spine_localization.py
 python main_vertebrae_localization.py
 python main_vertebrae_segmentation.py
 
 #copy the results 
-cp MedicalDataAugmentationTool-VerSe/training/output/* /MedicalDataAugmentationTool-VerSe/verse2019_dataset/images/output/
+cp -r output/* ../../output/
