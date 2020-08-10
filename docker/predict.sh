09171943d3f895dbfafc2de92fa8b41f2bf8d702
miruna20 <miruna.gafencu@yahoo.com>
2020-08-10 09:15:51 +0200

    inference pipeline with stage 3 only trained on cervical

--- docker/predict.sh
+++ docker/predict.sh
@@ -6,6 +6,7 @@ cd /scripts/
 python reorient_reference_to_rai.py --image_folder /data --output_folder /tmp/data_reoriented
 python main_spine_localization.py --image_folder /tmp/data_reoriented --setup_folder /tmp/ --model_files /models/spine_localization/model --output_folder /tmp/
 python main_vertebrae_localization.py --image_folder /tmp/data_reoriented --setup_folder /tmp/ --model_files /models/vertebrae_localization/model --output_folder /tmp/
+python middle_step.py --landmarksFolder /tmp/vertebrae_localization --reorientedImagesFolder /tmp/data_reoriented
 cp /tmp/vertebrae_localization/verse_landmarks/* /data/results/
 python main_vertebrae_segmentation.py --image_folder /tmp/data_reoriented --setup_folder /tmp/ --model_files /models/vertebrae_segmentation/model --output_folder /tmp/
 python reorient_prediction_to_reference.py --image_folder /tmp/vertebrae_segmentation --reference_folder /data --output_folder /data/results
